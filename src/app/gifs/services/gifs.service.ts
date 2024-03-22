f9e1308490938f4553dc6a817d4a82078dd9f3b4
GerardoZenteno <brallanzenteno@gmail.com>
2024-03-22 16:18:03 -0600

    added beter format and logic to search bar

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,9 +10,20 @@ export class GifService {
     return [...this._tagHistory];
   }
 
-  searchTag(tag: string): void {
-    this._tagHistory.unshift(tag);
-    console.log(this._tagHistory);
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
+    }
 
+    this._tagHistory.unshift(tag);
+    this._tagHistory = this._tagHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length == 0) return;
+    // this._tagHistory.unshift(tag);
+    this.organizeHistory(tag);
+    console.log(this._tagHistory);
   }
 }
