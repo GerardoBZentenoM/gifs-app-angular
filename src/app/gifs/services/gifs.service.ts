8c0fbee8e274932d0e976ee490d46d4bfc330cb7
GerardoZenteno <brallanzenteno@gmail.com>
2024-03-22 18:44:12 -0600

    Added http petition to obtain gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,15 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+const GIPHY_APY_KEY = 'fsEp7fGHQheRhpXnvuQ5VL8uXLhqzhq1';
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
   private _tagHistory: string[] = [];
-
-  constructor() {}
+  private serviceURL: string = 'https://api.giphy.com/v1/gifs';
+  public gifList: Gif[] = []
+  constructor(private http: HttpClient) {}
 
   get tagHistory() {
     return [...this._tagHistory];
@@ -20,10 +25,20 @@ export class GifService {
     this._tagHistory = this._tagHistory.splice(0, 10);
   }
 
-  searchTag(tag: string): void {
+  async searchTag(tag: string): Promise<void> {
     if (tag.length == 0) return;
     // this._tagHistory.unshift(tag);
     this.organizeHistory(tag);
     console.log(this._tagHistory);
+    const params = new HttpParams()
+      .set('api_key', GIPHY_APY_KEY)
+      .set('limit', '10')
+      .set('q', tag);
+    this.http
+      .get<SearchResponse>(`${this.serviceURL}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+        console.log('this.gifList: ', this.gifList);
+      });
   }
 }
